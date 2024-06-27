96a6679ea504fd2d7e4958a6ecf356b47c9c529f
Pedro_Barbosa <pedrovbb89@gmail.com>
2024-06-27 19:21:12 -0300

    feat: testing global return to render json

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
-class Contact < ApplicationRecord
+# frozen_string_literal: true
+
+class Contact < ApplicationRecord # :nodoc:
+  def author
+    'Pedro Barbosa'
+  end
+
+  # Redefines and add options to as_json in a way that all requests will
+  # add this lines
+  def as_json(_options = {})
+    super(methods: :author, root: true)
+  end
 end
