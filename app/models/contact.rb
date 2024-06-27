3cfa5a3e1997f1cbb4febbbd52c63aa6349e592a
Pedro_Barbosa <pedrovbb89@gmail.com>
2024-06-27 19:33:03 -0300

    feat: add relations and description method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord # :nodoc:
+  belongs_to :type
+
   def author
     'Pedro Barbosa'
+  end
+
+  def type_description
+    type.description
   end
 
   # Redefines and add options to as_json in a way that all requests will
   # add this lines
   def as_json(_options = {})
-    super(methods: :author, root: true)
+    super(root: true, methods: %i[type_description author])
   end
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,5 @@
-class Type < ApplicationRecord
+# frozen_string_literal: true
+
+class Type < ApplicationRecord # :nodoc:
+  has_many :contacts
 end
